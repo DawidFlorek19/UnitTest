ec2f52d2ae2c10ea716a72051e5caffe0f938a02
DawidFlorek19 <dawid.florek@sapure.com>
2023-08-09 13:28:09 +0200

    Add CircleCi Pipline

--- WeatherForecast/WeatherForecast/Test/WeatherForecastControllerTests.cs
+++ WeatherForecast/WeatherForecast/Test/WeatherForecastControllerTests.cs
@@ -1,6 +1,30 @@
-﻿namespace WeatherForecast.Test
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using WeatherForecast.Controllers;
+using Xunit;
+
+namespace WeatherForecast.Tests
 {
     public class WeatherForecastControllerTests
     {
+        private readonly WeatherForecastController _controller;
+
+        public WeatherForecastControllerTests()
+        {
+            var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<WeatherForecastController>();
+            _controller = new WeatherForecastController(logger);
+        }
+
+        [Fact]
+        public void TestGetWeatherForecast()
+        {
+            // Act
+            var result = _controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, result.Count());
+        }
     }
 }
